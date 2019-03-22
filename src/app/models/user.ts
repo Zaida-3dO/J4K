e38fb4cb6ae735b5fd6e2eeebf5077fe945b8521
Kooldeji <dejiakande33@gmail.com>
2019-03-22 17:13:56 +0100

    intial advocate register

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User{
+    id: number
+    name: string
+    phoneNumber: number
+    email: string
+    address: string
+    state: string
+    country: string
+    description: string
+}
