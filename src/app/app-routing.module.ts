ca7c5ea00f315266113893f3ce148d3b887b2f2d
Zaida-3dO <opsijay@gmail.com>
2019-03-22 18:35:54 +0100

    route thru land

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,28 +4,35 @@ import {LandingPageComponent} from './landing-page/landing-page.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {RegisterAdvocateComponent} from './auth/register-advocate/register-advocate.component';
 import {RegisterReporterComponent} from './auth/register-reporter/register-reporter.component';
+import {NewReportComponent} from './report/new-report/new-report.component';
 
 const routes: Routes = [
   {
-    path: '', component: LandingPageComponent
-  },
-  {
-    path: 'register',
+    path: '', component: LandingPageComponent,
     children: [
       {
-        path: '',
-        component: RegisterComponent
+        path: 'register',
+        children: [
+          {
+            path: '',
+            component: RegisterComponent
+          },
+          {
+            path: 'advocate',
+            component: RegisterAdvocateComponent
+          },
+          {
+            path: 'reporter',
+            component: RegisterReporterComponent
+          }
+        ]
       },
       {
-        path: 'advocate',
-        component: RegisterAdvocateComponent
-      },
-      {
-        path: 'reporter',
-        component: RegisterReporterComponent
+        path: 'new/report',
+        component: NewReportComponent
       }
     ]
-  }
+  },
 ];
 
 @NgModule({
