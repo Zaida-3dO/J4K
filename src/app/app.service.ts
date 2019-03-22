14541fb0541a3a5c270e45e24a3ab53593c525b9
Zaida-3dO <opsijay@gmail.com>
2019-03-22 22:50:07 +0100

    Added path promise to navbar

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-
+  path: Subject<string> = new BehaviorSubject(null);
+  path$ = this.path.asObservable();
   constructor() { }
+
+  setPath(path: string) {
+    this.path.next(path);
+  }
+
 }
