8524bc870ff329fb360040ea4e1adf439633f6db
Kooldeji <dejiakande33@gmail.com>
2019-03-22 23:13:57 +0100

    update navbars

--- src/app/report/report-list.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReportListDirective } from './report-list.directive';
-
-describe('ReportListDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ReportListDirective();
-    expect(directive).toBeTruthy();
-  });
-});
