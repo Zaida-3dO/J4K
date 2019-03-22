5fb21987e199f6446d8b01808cee71dbb0cf6d2d
Zaida-3dO <opsijay@gmail.com>
2019-03-22 19:01:58 +0100

    route to new report

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,18 @@ import {RegisterComponent} from './auth/register/register.component';
 import {RegisterAdvocateComponent} from './auth/register-advocate/register-advocate.component';
 import {RegisterReporterComponent} from './auth/register-reporter/register-reporter.component';
 import {NewReportComponent} from './report/new-report/new-report.component';
+import {ReporterComponent} from './reporter/reporter.component';
+import {ReporterMainComponent} from './reporter/reporter-main/reporter-main.component';
+import {LandingPageMainComponent} from './landing-page/landing-page-main/landing-page-main.component';
 
 const routes: Routes = [
   {
     path: '', component: LandingPageComponent,
     children: [
+      {
+        path: '',
+        component: LandingPageMainComponent
+      },
       {
         path: 'register',
         children: [
@@ -27,12 +34,22 @@ const routes: Routes = [
           }
         ]
       },
+    ]
+  },
+  {
+    path: 'reporter',
+    component: ReporterComponent,
+    children: [
+      {
+        path: '',
+        component: ReporterMainComponent
+      },
       {
         path: 'new/report',
         component: NewReportComponent
       }
     ]
-  },
+  }
 ];
 
 @NgModule({
