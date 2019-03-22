5e88956a8e7649a5b659de13cbcfb496457a00c2
Zaida-3dO <opsijay@gmail.com>
2019-03-22 21:39:05 +0100

    Done with router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,46 +8,46 @@ import {NewReportComponent} from './report/new-report/new-report.component';
 import {ReporterComponent} from './reporter/reporter.component';
 import {ReporterMainComponent} from './reporter/reporter-main/reporter-main.component';
 import {LandingPageMainComponent} from './landing-page/landing-page-main/landing-page-main.component';
+import {LoginComponent} from './auth/login/login.component';
+import {UpdateProfileComponent} from './auth/update-profile/update-profile.component';
+import {ReportMainComponent} from './report/report-main/report-main.component';
+import {AdvocateComponent} from './advocate/advocate.component';
+import {AdvocateMainComponent} from './advocate/advocate-main/advocate-main.component';
+import {ReportListComponent} from './report/report-list/report-list.component';
 
 const routes: Routes = [
   {
     path: '', component: LandingPageComponent,
     children: [
-      {
-        path: '',
-        component: LandingPageMainComponent
-      },
+      {path: '', component: LandingPageMainComponent},
       {
         path: 'register',
         children: [
-          {
-            path: '',
-            component: RegisterComponent
-          },
-          {
-            path: 'advocate',
-            component: RegisterAdvocateComponent
-          },
-          {
-            path: 'reporter',
-            component: RegisterReporterComponent
-          }
+          {path: '', component: RegisterComponent},
+          {path: 'advocate', component: RegisterAdvocateComponent},
+          {path: 'reporter', component: RegisterReporterComponent}
         ]
       },
+      {path: 'login', component: LoginComponent},
+      {path: 'updateAccount', component: UpdateProfileComponent}
     ]
   },
   {
-    path: 'reporter',
-    component: ReporterComponent,
+    path: 'reporter', component: ReporterComponent,
     children: [
-      {
-        path: '',
-        component: ReporterMainComponent
-      },
-      {
-        path: 'new/report',
-        component: NewReportComponent
-      }
+      {path: '', component: ReporterMainComponent},
+      {path: ':reportId', component: ReportMainComponent},
+      {path: 'new/report', component: NewReportComponent},
+      {path: 'updateAccount', component: UpdateProfileComponent}
+    ]
+  },
+  {
+    path: 'advocate', component: AdvocateComponent,
+    children: [
+      {path: '', component: AdvocateMainComponent},
+      {path: ':content', component: ReportListComponent},
+      {path: ':reportId', component: ReportMainComponent},
+      {path: 'updateAccount', component: UpdateProfileComponent}
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,24 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterAdvocateComponent } from './auth/register-advocate/register-advocate.component';
-import { RegisterReporterComponent } from './auth/register-reporter/register-reporter.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { AdvocateComponent } from './advocate/advocate.component';
-import { ReporterComponent } from './reporter/reporter.component';
-import { UpdateProfileComponent } from './auth/update-profile/update-profile.component';
-import { ReportMainComponent } from './report/report-main/report-main.component';
-import { NewReportComponent } from './report/new-report/new-report.component';
-import { ReporterMainComponent } from './reporter/reporter-main/reporter-main.component';
-import { AdvocateMainComponent } from './advocate/advocate-main/advocate-main.component';
-import { ReportListDirective } from './report/report-list.directive';
-import { ReportCardDirective } from './report/report-card.directive';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LandingPageMainComponent } from './landing-page/landing-page-main/landing-page-main.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterAdvocateComponent} from './auth/register-advocate/register-advocate.component';
+import {RegisterReporterComponent} from './auth/register-reporter/register-reporter.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {AdvocateComponent} from './advocate/advocate.component';
+import {ReporterComponent} from './reporter/reporter.component';
+import {UpdateProfileComponent} from './auth/update-profile/update-profile.component';
+import {ReportMainComponent} from './report/report-main/report-main.component';
+import {NewReportComponent} from './report/new-report/new-report.component';
+import {ReporterMainComponent} from './reporter/reporter-main/reporter-main.component';
+import {AdvocateMainComponent} from './advocate/advocate-main/advocate-main.component';
+import {ReportCardDirective} from './report/report-card.directive';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LandingPageMainComponent} from './landing-page/landing-page-main/landing-page-main.component';
+import {ReportListComponent} from './report/report-list/report-list.component';
 
 @NgModule({
-  // TODO report list directive
   declarations: [
     AppComponent,
     LoginComponent,
@@ -36,9 +35,9 @@ import { LandingPageMainComponent } from './landing-page/landing-page-main/landi
     NewReportComponent,
     ReporterMainComponent,
     AdvocateMainComponent,
-    ReportListDirective,
     ReportCardDirective,
-    LandingPageMainComponent
+    LandingPageMainComponent,
+    ReportListComponent
   ],
   imports: [
     BrowserModule,
