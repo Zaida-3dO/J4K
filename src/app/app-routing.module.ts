15d9629fd8850a53bc67b487ac9ba3b4b224fa78
Zaida-3dO <opsijay@gmail.com>
2019-03-22 17:06:19 +0100

    register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {RegisterAdvocateComponent} from './auth/register-advocate/register-advocate.component';
+import {RegisterReporterComponent} from './auth/register-reporter/register-reporter.component';
 
 const routes: Routes = [
   {
     path: '', component: LandingPageComponent
+  },
+  {
+    path: 'register',
+    children: [
+      {
+        path: '',
+        component: RegisterComponent
+      },
+      {
+        path: 'advocate',
+        component: RegisterAdvocateComponent
+      },
+      {
+        path: 'reporter',
+        component: RegisterReporterComponent
+      }
+    ]
   }
 ];
 
