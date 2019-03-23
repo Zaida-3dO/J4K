f362feedb03bf1359b19087550ae6a1a173c6c26
Kooldeji <dejiakande33@gmail.com>
2019-03-23 02:26:34 +0100

    update

--- src/app/mock-reports.ts
+++ src/app/mock-reports.ts
@@ -2,13 +2,13 @@ import {Report} from './models/report';
 
 const CONTENT = 'Lorem ipsum dolor sit amet, consetetur sadipscing esed diam nonumy eirmod tempor invidunt ut';
 export const REPORTS: Report[] = [
-  { title: 'Death Squad killing of street children', content: CONTENT, persons: [], tags: []},
-  { title: 'Child arrested for exam malpractice', content: CONTENT, persons: [], tags: []},
-  { title: 'Child Raped and sent out of house!', content: CONTENT, persons: [], tags: []},
-  { title: 'Child maltreated by school teachers', content: CONTENT, persons: [], tags: []},
-  { title: 'Child forced to marry and dropout of school', content: CONTENT, persons: [], tags: []},
-  { title: 'Child cheated of legal inheritance', content: CONTENT, persons: [], tags: []},
-  { title: 'Child Forced Labour!!', content: CONTENT, persons: [], tags: []},
-  { title: 'Child arrested for stealing food to eat', content: CONTENT, persons: [], tags: []},
-  { title: 'Child sentenced to Life imprisonment!', content: CONTENT, persons: [], tags: []}
+  { title: 'Death Squad killing of street children', content: CONTENT, persons: [], tags: [], status: 1},
+  { title: 'Child arrested for exam malpractice', content: CONTENT, persons: [], tags: [], status: 2},
+  { title: 'Child Raped and sent out of house!', content: CONTENT, persons: [], tags: [], status: 0},
+  { title: 'Child maltreated by school teachers', content: CONTENT, persons: [], tags: [], status: 2},
+  { title: 'Child forced to marry and dropout of school', content: CONTENT, persons: [], tags: [], status: 1},
+  { title: 'Child cheated of legal inheritance', content: CONTENT, persons: [], tags: [], status: 0},
+  { title: 'Child Forced Labour!!', content: CONTENT, persons: [], tags: [], status: 2},
+  { title: 'Child arrested for stealing food to eat', content: CONTENT, persons: [], tags: [], status: 0},
+  { title: 'Child sentenced to Life imprisonment!', content: CONTENT, persons: [], tags: [], status: 1}
 ];
