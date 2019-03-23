7752ad902b2e76782c21feb7b8b739f8094c2ca1
koudura <kouduraninci@gmail.com>
2019-03-23 10:26:08 +0100

    activities phase 3

--- src/app/mock-reports.ts
+++ src/app/mock-reports.ts
@@ -3,8 +3,8 @@ import {Caption} from './models/caption';
 
 const CONTENT = 'Lorem ipsum dolor sit amet, consetetur sadipscing esed diam nonumy eirmod tempor invidunt ut';
 
-export const REPORTS: Report[] = [
-  { title: 'Death Squad killing of street children', content: CONTENT, persons: [], tags: [], status: 0, date: '22 March, 2019',
+export const REPORTS: any[] = [
+  { title: 'Death Squad killing of street children', content: CONTENT, persons: [], tags: [], status: 1, date: '22 March, 2019',
     caption: CONTENT},
   { title: 'Child arrested for exam malpractice', content: CONTENT, persons: [], tags: [], status: 1, date: '19 March, 2019',
     caption: CONTENT},
@@ -14,12 +14,12 @@ export const REPORTS: Report[] = [
     caption: CONTENT},
   { title: 'Child forced to marry and dropout of school', content: CONTENT, persons: [], tags: [], status: 1, date: '29 January, 2019',
     caption: CONTENT},
-  { title: 'Child cheated of legal inheritance', content: CONTENT, persons: [], tags: [], status: 2, date: '28 January, 2019',
+  { title: 'Child cheated of legal inheritance', content: CONTENT, persons: [], tags: [], status: 1, date: '28 January, 2019',
     caption: CONTENT },
   { title: 'Child Forced Labour!!', content: CONTENT, persons: [], tags: [], status: 2, date: '12 January, 2019',
     caption: CONTENT},
-  { title: 'Child arrested for stealing food to eat', content: CONTENT, persons: [], tags: [], status: 1, date: '24 December, 2018',
+  { title: 'Child arrested for stealing food to eat', content: CONTENT, persons: [], tags: [], status: 0, date: '24 December, 2018',
     caption: CONTENT},
-  { title: 'Child sentenced to Life imprisonment!', content: CONTENT, persons: [], tags: [], status: 2, date: '13 December, 2018',
+  { title: 'Child sentenced to Life imprisonment!', content: CONTENT, persons: [], tags: [], status: 1, date: '13 December, 2018',
     caption: CONTENT}
 ];
